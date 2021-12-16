0287cbb607ec89112cdf608ca16bf42c46a16e1c
Tunarwn <96257156+Tunarwn@users.noreply.github.com>
2021-12-16 20:29:44 +0300

    c

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturna <tturna@student.42kocaeli.com.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 18:27:23 by tturna            #+#    #+#             */
+/*   Updated: 2021/11/30 19:32:33 by tturna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	char	a;
+	char	b;
+
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		ft_putchar(a / 10 + '0');
+		ft_putchar(a % 10 + '0');
+		ft_putchar(' ');
+		ft_putchar(b / 10 + '0');
+		ft_putchar(b % 10 + '0');
+		if (a < 98)
+			write(1, ", ", 2);
+		if (++b > 99)
+			b = ++a + 1;
+	}
+}
