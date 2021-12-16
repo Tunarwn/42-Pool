c12229a7f7c393171fb0f0dea2bb2eea57fba36e
Tunarwn <96257156+Tunarwn@users.noreply.github.com>
2021-12-16 20:34:41 +0300

    C Piscine C_07

---
+++ C_07/ex01/ft_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturna <tturna@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 17:15:48 by tturna            #+#    #+#             */
+/*   Updated: 2021/12/15 17:15:54 by tturna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*p;
+	int	i;
+
+	if (min >= max)
+		return (0);
+	p = (int *)malloc(sizeof(int) * (max - min));
+	if (p == NULL)
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		p[i] = min;
+		i++;
+		min++;
+	}
+	return (p);
+}
---
+++ C_07/ex03/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tturna <tturna@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 17:27:35 by tturna            #+#    #+#             */
+/*   Updated: 2021/12/15 17:27:43 by tturna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*s;
+	int		i;
+	int		j;
+	int		c;
+
+	s = (char *)malloc(sizeof(strs));
+	i = 0;
+	c = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j] != '\0')
+		{
+			s[c++] = strs[i][j++];
+		}
+		j = 0;
+		while (sep[j] != '\0' && i != (size - 1))
+		{
+			s[c++] = sep[j++];
+		}
+		i++;
+	}
+	s[c] = '\0';
+	return (s);
+}
